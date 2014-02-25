c7611c5c572aecfcf30ae18e429d5baa2f195633
agatov <agatovs@gmail.com>
2014-02-25 05:39:37 +0400

    api method changes

--- app/controllers/api/clients_controller.rb
+++ app/controllers/api/clients_controller.rb
@@ -3,6 +3,12 @@ class Api::ClientsController < ApplicationController
     @client = Client.new params[:client]
 
     if @client.save
+
+      if params[:first_comment]
+        @comment = @client.comments.build params[:first_comment]
+        @comment.save
+      end
+
       render json: {status: :ok}
     else
       render json: {status: :error}
